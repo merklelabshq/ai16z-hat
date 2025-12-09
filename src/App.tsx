6d084d2248a7495abb905f1a25851a7b5ab84245
Kevin Paul Jacob <kpjtofficial@gmail.com>
2025-12-09 16:27:23 +0530

    build warning fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PhotoEditor from './components/PhotoEditor/PhotoEditor';
+import "./App.css";
+import PhotoEditor from "./components/PhotoEditor/PhotoEditor";
 
 function App() {
   return (
     <div className="App">
-      <PhotoEditor/>
+      <PhotoEditor />
     </div>
   );
 }
